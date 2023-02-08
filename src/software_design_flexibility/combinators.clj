b5ac0329244e575bdafd93bb41b781378522c6a6
Akshay Gupta <akshaygrkesh@gmail.com>
2023-02-08 15:17:59 +0530

    Exercise 2.1

--- src/software_design_flexibility/combinators.clj
+++ src/software_design_flexibility/combinators.clj
@@ -30,10 +30,22 @@
              last
              second)))))
 
+(defn with-arity
+  "Wraps a function with its arity metadata"
+  [f arity]
+  (with-meta f {:arity arity}))
+
 (defn compose [f g]
-  (fn [& args] (->> args
-                   (apply g)
-                   f)))
+  (let [arity-f (arity f)
+        arity-g (arity g)]
+    (assert (= arity-f 1))
+    (with-arity
+      (fn [& args]
+        (assert (= (count args) arity-g))
+        (->> args
+             (apply g)
+             f))
+      arity-g)))
 
 (defn my-iterate
   "A hof which recurs a function n times"
@@ -45,19 +57,29 @@
 (defn parallel-combine
   "A hof which combines the result of calling f and g"
   [combinator f g]
-  (fn [& args] (combinator (apply f args) (apply g args))))
+  (let [arity-f (arity f)
+        arity-g (arity g)
+        arity-c (arity combinator)]
+    (assert (= arity-f arity-g))
+    (assert (= arity-c 2))
+    (with-arity
+      (fn [& args]
+        (assert (= (count args) arity-f))
+        (combinator (apply f args) (apply g args)))
+      arity-f)))
 
 (defn spread-combine
   "A hof which splits args between f and g according to their arities and combines the results"
   [combinator f g]
   (let [arity-f     (arity f)
         arity-g     (arity g)
-        total-arity (+ arity-f arity-g)]
-    (with-meta
+        total-arity (+ arity-f arity-g)
+        arity-c     (arity combinator)]
+    (assert (= arity-c 2))
+    (with-arity
       (fn [& args]
                  (assert (= (count args) total-arity))
-                 (print arity-f arity-g total-arity)
                  (combinator
                   (apply f (take arity-f args))
                   (apply g (take arity-g (drop arity-f args)))))
-      {:arity total-arity})))
+      total-arity)))
